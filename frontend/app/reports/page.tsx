32c854fb1d961c711de3b4e2aeb7a7a4d2fb417e
Rawad Karam <rawad.karam663@gmail.com>
2026-04-08 12:43:03 -0400

    feat: Label mock pages as such in UI

--- frontend/app/reports/page.tsx
+++ frontend/app/reports/page.tsx
@@ -1,12 +1,23 @@
 import { BarChart3 } from "lucide-react";
+import { CSSProperties } from "react";
 
 import { AppShell } from "@/components/app-shell";
 import { MockFeaturePage } from "@/components/mock/mock-feature-page";
 import { reportsPageMock } from "@/lib/mocks/reports";
 
+const style: CSSProperties = {
+  position: "absolute",
+  top: "40%",
+  left: "50%",
+  fontSize: 100,
+  transform: "rotate(-45deg)",
+  color: "red",
+};
+
 export default function Reports() {
   return (
     <AppShell>
+      <h1 style={style}>MOCK</h1>
       <MockFeaturePage icon={BarChart3} data={reportsPageMock} />
     </AppShell>
   );
